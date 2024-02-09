1562f2e3f34c3c7649cf5e3f2e2b32177bf46a5e
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-02-09 23:36:06 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -83,7 +83,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 *Hello {} !*
-✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
+✪ I'm an Raiden Shogun management bot [✨](https://telegra.ph/file/0501b90a698532c7e6172.jpg)
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
